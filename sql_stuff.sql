ffe682f62189468091dba5f75b6f36c5033a9679
Gael Sonney <gael.sonney@siteminder.com>
2016-05-17 10:53:51 +1000

    fixed sql

--- sql_stuff.sql
+++ sql_stuff.sql
@@ -1,12 +1,17 @@
 
 # Delete reservation
 
-SET @res_id = 9;
-delete from guest where `RESERVATION_ID`= @res_id;
-delete from profile where `RESERVATION_ID`=@res_id;
-delete from `reservation_document` where `RESERVATION_ID`=@res_id;
-delete from room_rate where `ROOM_STAY_ID` in (select id from `room_stay` where `RESERVATION_ID`=@res_id);
-delete from `room_stay` where `RESERVATION_ID`= @res_id;
-delete from reservation where id=@res_id;
+Use `siteminder_dev`;
 
-	
+START TRANSACTION;
+
+SET @res_id = '8';
+
+	delete from guest where `RESERVATION_ID`= @res_id;
+	delete from profile where `RESERVATION_ID`= @res_id;
+	delete from `reservation_document` where `RESERVATION_ID`= @res_id;
+	delete from room_rate where `ROOM_STAY_ID` in (select id from `room_stay` where `RESERVATION_ID`= @res_id);
+	delete from `room_stay` where `RESERVATION_ID`= @res_id;
+	delete from reservation where id= @res_id;
+
+COMMIT;
